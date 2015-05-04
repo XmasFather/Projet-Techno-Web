96b755b0fe1172974cee6a4b7a174bb1a37cb00d
Kill3rdu77 <etienne.wehowski@gmail.com>
2015-05-04 17:29:21 +0200

    Legere modification

--- Projet_Techno_Web/PROJET_Wehowski_Marolleau/Javascript/formulaire.js
+++ Projet_Techno_Web/PROJET_Wehowski_Marolleau/Javascript/formulaire.js
@@ -2,13 +2,13 @@
   chmin = document.getElementById("spawn");
   choix=chmin.selectedIndex;
   path=chmin.options[choix].value;
-  if (path >= 0 && path <= 2) { var txt="route peu intéressante";} 
-  else if (path >= 3 && path <= 7) { var txt="grimdale / The Lost Castle / Yongton Abbey / Arbreton / Aspermont / Lazerville / Water Spire / Castle Byesford";}
-  else if (path >= 8 && path <= 16) { var txt="Pravus / Yawpton / Logging Camp / Whitehaven / Devil's Castle / McLovinville / The Forest / Sanctuary of Peace / Crowmure / Ghost Fleet ou Fort Saward ou Mantle ou Hell Tree";}
-  else if (path >= 17 && path <=22) { var txt="Portsmouth / Consolio University / Evergreen Manor et Evergreen Dam / Whitehaven / Paluster / Frostbain / Crymoore Pond /Dark Mansion ou Necromancer's tower";}
-  else if (path >= 23 && path <=29) { var txt="Romero (Camp Bell avant si vous êtes proches) / Huntsgrove / Spire / Ridgevale / Ravine / Fort Erie / Knoxmoor Inn / Shrine of the Dusk / Camp Kharj / Sunken Tower / Al Hasa / Fort Kharj";}
-  else if (path >= 30 && path <=35) { var txt="Carmi / Saens / St.Roseluck Island / Afaya / Anvalia / Tristitia (attention à pas ne pas vous perdre!) / Red Demon Temple / Grayvale (uniquement si vous avez un bouton) / Eillom / Camp Azara / Wizard Tower ou Cave of the Dead Lady";}
-  else if (path >= 36 && path <=40) { var txt="Geuten / (optionnel : Grandfather's Curse - Mount Longton - Jacsebalon Mansion - Heartless Mansion) / Worthington / Cole Castle / Aeternalis Cathedral / Fort Rockwood et Forth wind / Pileus / Meteor Crater / Agni Ignis";}
+  if (path >= 0 && path <= 2) { var txt=" Nous vous déconseillons ces spawns, ils sont peu intéressant";} 
+  else if (path >= 3 && path <= 7) { var txt=" Grimdale / The Lost Castle / Yongton Abbey / Arbreton / Aspermont / Lazerville / Water Spire / Castle Byesford";}
+  else if (path >= 8 && path <= 16) { var txt=" Pravus / Yawpton / Logging Camp / Whitehaven / Devil's Castle / McLovinville / The Forest / Sanctuary of Peace / Crowmure / Ghost Fleet ou Fort Saward ou Mantle ou Hell Tree";}
+  else if (path >= 17 && path <=22) { var txt=" Portsmouth / Consolio University / Evergreen Manor et Evergreen Dam / Whitehaven / Paluster / Frostbain / Crymoore Pond /Dark Mansion ou Necromancer's tower";}
+  else if (path >= 23 && path <=29) { var txt=" Romero (Camp Bell avant si vous êtes proches) / Huntsgrove / Spire / Ridgevale / Ravine / Fort Erie / Knoxmoor Inn / Shrine of the Dusk / Camp Kharj / Sunken Tower / Al Hasa / Fort Kharj";}
+  else if (path >= 30 && path <=35) { var txt=" Carmi / Saens / St.Roseluck Island / Afaya / Anvalia / Tristitia (attention à pas ne pas vous perdre!) / Red Demon Temple / Grayvale (uniquement si vous avez un bouton) / Eillom / Camp Azara / Wizard Tower ou Cave of the Dead Lady";}
+  else if (path >= 36 && path <=40) { var txt=" Geuten / (optionnel : Grandfather's Curse - Mount Longton - Jacsebalon Mansion - Heartless Mansion) / Worthington / Cole Castle / Aeternalis Cathedral / Fort Rockwood et Forth wind / Pileus / Meteor Crater / Agni Ignis";}
   
   alert('Votre chemin proposé est :'+txt);
 }
