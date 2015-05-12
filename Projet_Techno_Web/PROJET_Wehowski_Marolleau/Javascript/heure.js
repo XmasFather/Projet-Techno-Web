dbbdf124e7697c342d49d887dcf9a23f662d67e1
Kill3rdu77 <etienne.wehowski@gmail.com>
2015-05-12 19:31:01 +0200

    Javascript heure connexion

--- Projet_Techno_Web/PROJET_Wehowski_Marolleau/Javascript/heure.js
+++ Projet_Techno_Web/PROJET_Wehowski_Marolleau/Javascript/heure.js
@@ -1,7 +1,22 @@
-function heure(){
-  var d = New Date();
-  var h = d.getHours();
-  var m = d.getMinutes();
-  var s = d.getSecond();
-  var date = h + m + s;
+function heure{
+	heure = new heure;
+		h = heure.getHours();
+        if(h<10)
+        {
+        h = "0"+h;
+        }
+		m = heure.getMinutes();
+        if(m<10)
+        {
+        m = "0"+m;
+        }
+		s = heure.getSeconds();
+        if(s<10)
+        {
+        s = "0"+s;
+        }
+	result = "Vous vous êtes connecté à '+h+':'+m+':'+s+'";
+	document.getElementById(connexion)=result;
 }
+
+
